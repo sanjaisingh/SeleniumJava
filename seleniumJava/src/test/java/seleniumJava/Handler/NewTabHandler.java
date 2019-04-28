b56c1a02fc7f5b2bfcdae063237058de277db921
Sanjay Singh <Sanjay Singh@DESKTOP-HV678ES>
2019-04-28 20:51:29 +0530

    Alert Handling done

--- seleniumJava/src/test/java/seleniumJava/Handler/NewTabHandler.java
+++ seleniumJava/src/test/java/seleniumJava/Handler/NewTabHandler.java
@@ -3,6 +3,7 @@ package seleniumJava.Handler;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -11,14 +12,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NewTabHandler {
 
-	public static void main(String[] args) throws Exception 
+	public static WebDriver driver;
+	
+	public static void main(String[] args) throws Exception
 	{
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+		//getWinHandle();
+		//getTabHandle();
+		getAlertHandle();
+	}
+
 //Multiple Window handlers
+	public static void getWinHandle() throws Exception
+	{
 		driver.get("https://www.naukri.com");
 		String parantWindow = driver.getWindowHandle();		
 		for(String childWindows : driver.getWindowHandles())
@@ -34,8 +43,11 @@ public class NewTabHandler {
 		System.out.println(driver.getTitle() +" : "+ parantWindow);
 		Thread.sleep(5000);
 		driver.close();
-		
+	}	
+	
 //Open links in new tab
+	public static void getTabHandle() throws Exception
+	{
 		driver.get("https://www.gmail.com");
 		List<WebElement> list = driver.findElements(By.tagName("a"));
 		System.out.println(list.size());
@@ -61,6 +73,28 @@ public class NewTabHandler {
 		Thread.sleep(5000);
 		driver.close();
 	}
+	
+//Alert handling
+	public static void getAlertHandle() throws Exception
+	{
+		driver.get("https://www.toolsqa.com/handling-alerts-using-selenium-webdriver/");
+	//Simple Alert	
+		driver.findElement(By.xpath("//*[@id='content']/p[4]/button")).click();
+		Alert sAlert = driver.switchTo().alert();
+		System.out.println("Simple Alert Title: " + sAlert.getText());
+		sAlert.accept();
+	//Confirmation Alert
+		driver.findElement(By.xpath("//*[@id='content']/p[8]/button")).click();
+		Alert cAlert = driver.switchTo().alert();
+		System.out.println("Confirmation Alert Title:" + cAlert.getText());
+		cAlert.accept();
+	//Prompt Alert
+		driver.findElement(By.xpath("//*[@id='content']/p[11]/button")).click();
+		Alert pAlert = driver.switchTo().alert();
+		System.out.println("Prompt Alert Title:" + pAlert.getText());
+		pAlert.sendKeys("Testing selenium alert");
+		pAlert.accept();
+		Thread.sleep(5000);
+		driver.close();
+	}
 }
-
-
